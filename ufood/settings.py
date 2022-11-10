06f7d1b07ce8f308181e841edd7aadc0ce134cc0
fpcoutinho <fpcoutinho5@gmail.com>
2022-11-10 11:06:28 -0300

    user base

--- ufood/settings.py
+++ ufood/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'ufood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'assets'),
 ]
--- ufood/views.py
+++ ufood/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.conf import settings
 
 def homepage(request):
-    return render(request, settings.BASE_DIR/'templates/base.html')
+    return render(request, settings.BASE_DIR/'templates/user_base.html')
